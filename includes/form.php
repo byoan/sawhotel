2e0dbf497b36d6277f3ea74ac3692eb43316e7f6
Yoan Ballesteros <b.yoan@live.fr>
2018-03-16 16:47:14 +0100

    Update code

---
+++ includes/form.php
@@ -0,0 +1,57 @@
+<body>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <h1 class="navbar-brand mb-0 h1">Analyse Multicritères - RankHotel</h1>
+    </nav>
+    <form method="POST">
+    <table class="table">
+        <tr>
+            <th></th>
+            <th>Prix</th>
+            <th>Distance</th>
+            <th>NbEt</th>
+        </tr>
+        <tr>
+            <td>Poids</td>
+            <td><input required type="text" name="prix[]"></td>
+            <td><input required type="text" name="distance[]"></td>
+            <td><input required type="text" name="nbet[]"></td>
+        </tr>
+        <tr>
+            <td>Préférence</td>
+            <td><input required type="text" name="prix[]"></td>
+            <td><input required type="text" name="distance[]"></td>
+            <td><input required type="text" name="nbet[]"></td>
+        </tr>
+        </table>
+        <input class="btn btn-info" type="submit" value="Submit">
+    </form>
+    <!-- <h1>Résultats</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">IdH</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Distance</th>
+                <th scope="col">NbEt</th>
+                <th scope="col">Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table> -->
+</body>
+<html>
--- index.php
+++ index.php
@@ -7,42 +7,56 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<body>
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-        <h1 class="navbar-brand mb-0 h1">Analyse Multicritères - RankHotel</h1>
-    </nav>
-    <form action="/action_page.php">
-        <label>Poids</label><input type="text" name="poids"><input type="text" name="poids"><input type="text" name="poids"><br/>
-        <label>Préférence</label><input type="text" name="preference"><input type="text" name="preference"><input type="text" name="preference"><input type="text" name="preference"><br><br>
-        <input type="submit" value="Submit">
-    </form>
-    <h1>Résultats</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">IdH</th>
-                <th scope="col">Prix</th>
-                <th scope="col">Distance</th>
-                <th scope="col">NbEt</th>
-                <th scope="col">Score</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-</body>
-<html>
+
+<?php
+if (!empty($_POST)) {
+    $coefs = array();
+    $values = array();
+    foreach ($_POST as $key => $value) {
+        $coefs[] = (int)$value[0];
+        $values[$key] = (int)$value[1];
+    }
+    $calculatedCoefs = array();
+    foreach ($coefs as $key => $value) {
+        $calculatedCoefs[$key] = $value/(array_sum($coefs));
+    }
+    if (array_sum($calculatedCoefs) > 1) {
+        die('erreur dans la somme des coefs');
+    }
+
+    $db = new PDO('mysql:dbname=sawhotel;host=127.0.0.1', 'root', 'root');
+    $query = "DROP VIEW IF EXISTS Hotel_Pond;";
+    $statement = $db->prepare($query);
+    $statement->execute();
+
+    $query = "CREATE VIEW Hotel_Pond
+              as
+              SELECT IdH, TRUNCATE($calculatedCoefs[0] * Prix_Norm, 3) Prix_Pond, TRUNCATE($calculatedCoefs[1] * Distance_Norm, 2) Distance_Pond, TRUNCATE($calculatedCoefs[2] * NbEt_Norm, 3) NbEt_Pond
+              FROM Hotel_Norm;";
+    $statement = $db->prepare($query);
+    $statement->execute();
+
+    $query = "SELECT H.IdH, H.Prix, H.Distance, H.NbEt, S.Score
+    FROM Hotel H, Hotel_Score S
+    WHERE H.IdH = S.IdH
+    ORDER BY S.Score Desc;";
+
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<div class="container"><h2>Résultats</h2><table class="table">';
+    echo '<tr>';
+    echo '<th>IdH</th><th>Prix</th><th>Distance</th><th>NbEt</th><th>Score</th>';
+    echo '</tr>';
+    foreach ($result as $key => $value) {
+        echo '<tr>';
+        foreach ($value as $id => $val) {
+            echo '<td>' . $val . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table></div>';
+} else {
+    include('includes/form.php');
+}
