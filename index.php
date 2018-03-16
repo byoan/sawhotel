4a374ed86e99403903b35b54a6ad62ebc395c5bc
Antoine Sirven <antoinesirvenpro@gmail.com>
2018-03-16 10:06:24 +0100

    Add index.html

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Analyse Multicritères - RankHotel</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <h1 class="navbar-brand mb-0 h1">Analyse Multicritères - RankHotel</h1>
+    </nav>
+    <form action="/action_page.php">
+        <label>Poids</label><input type="text" name="poids"><input type="text" name="poids"><input type="text" name="poids"><br/>
+        <label>Préférence</label><input type="text" name="preference"><input type="text" name="preference"><input type="text" name="preference"><input type="text" name="preference"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    <h1>Résultats</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">IdH</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Distance</th>
+                <th scope="col">NbEt</th>
+                <th scope="col">Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+<html>
